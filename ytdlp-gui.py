e7905891a4a2d85d1ed352b70a50354378179962
sridip-de <sridipdey630@gmail.com>
2025-10-28 12:56:04 +0530

    Code optimzed and Fixed yt-dlp commands

--- ytdlp-gui.py
+++ ytdlp-gui.py
@@ -3,24 +3,54 @@ import gi
 import os
 import threading
 import subprocess
-import json
 from pathlib import Path
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib, Gdk
 
 class YtDlpGUI(Gtk.ApplicationWindow):
+    QUALITY_OPTIONS = {
+        "video": [("best", "Best Available"), ("1080", "1080p"), ("720", "720p"), 
+                  ("480", "480p"), ("360", "360p")],
+        "audio": [("best", "Best Available"), ("192", "192 kbps"), ("128", "128 kbps"), ("64", "64 kbps")]
+    }
+    
+    FORMAT_OPTIONS = {
+        "video": [("best", "Best (auto)"), ("mp4", "MP4"), ("mkv", "MKV"), ("webm", "WebM")],
+        "audio": [("best", "Best (auto)"), ("mp3", "MP3"), ("m4a", "M4A"), ("wav", "WAV"), ("opus", "Opus")]
+    }
+    
+    SUB_LANGS = [("en", "English"), ("all", "All Available"), ("es", "Spanish"), 
+                 ("fr", "French"), ("de", "German"), ("hi", "Hindi")]
+    
     def __init__(self, app):
         super().__init__(application=app, title="yt-dlp GUI Manager")
-        self.set_default_size(700, 800)
+        self.set_default_size(700, 850)
         self.set_border_width(10)
         self.set_icon_name("application-x-executable")
         
         self.download_thread = None
         self.process = None
-        
         self.setup_ui()
         
+    def create_labeled_combo(self, label_text, options, active_id="best"):
+        """Helper to create labeled comboboxes"""
+        label = Gtk.Label(label=label_text, xalign=0)
+        combo = Gtk.ComboBoxText()
+        for opt_id, opt_label in options:
+            combo.append(opt_id, opt_label)
+        combo.set_active_id(active_id)
+        return label, combo
+    
+    def create_frame(self, title, spacing=8):
+        """Helper to create styled frames"""
+        frame = Gtk.Frame(label=title)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=spacing)
+        for margin in ['top', 'bottom', 'start', 'end']:
+            getattr(box, f'set_margin_{margin}')(10)
+        frame.add(box)
+        return frame, box
+    
     def setup_ui(self):
         main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         self.add(main_box)
@@ -48,106 +78,56 @@ class YtDlpGUI(Gtk.ApplicationWindow):
         main_box.pack_start(type_label, False, False, 0)
         main_box.pack_start(type_box, False, False, 0)
         
-        # Video Quality Section
-        self.video_frame = Gtk.Frame(label="Video Settings")
-        video_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
-        video_box.set_margin_top(10)
-        video_box.set_margin_bottom(10)
-        video_box.set_margin_start(10)
-        video_box.set_margin_end(10)
+        # Video Settings Frame
+        self.video_frame, video_box = self.create_frame("Video Settings")
         
-        quality_label = Gtk.Label(label="Quality:", xalign=0)
-        self.quality_combo = Gtk.ComboBoxText()
-        self.quality_combo.append("best", "Best Available")
-        self.quality_combo.append("1080", "1080p")
-        self.quality_combo.append("720", "720p")
-        self.quality_combo.append("480", "480p")
-        self.quality_combo.append("360", "360p")
-        self.quality_combo.set_active_id("best")
-        video_box.pack_start(quality_label, False, False, 0)
+        v_quality_label, self.quality_combo = self.create_labeled_combo(
+            "Quality:", self.QUALITY_OPTIONS["video"], "best")
+        video_box.pack_start(v_quality_label, False, False, 0)
         video_box.pack_start(self.quality_combo, False, False, 0)
         
-        v_format_label = Gtk.Label(label="Video Format:", xalign=0)
-        self.v_format_combo = Gtk.ComboBoxText()
-        self.v_format_combo.append("best", "Best (auto)")
-        self.v_format_combo.append("mp4", "MP4")
-        self.v_format_combo.append("mkv", "MKV")
-        self.v_format_combo.append("webm", "WebM")
-        self.v_format_combo.set_active_id("best")
+        v_format_label, self.v_format_combo = self.create_labeled_combo(
+            "Video Format:", self.FORMAT_OPTIONS["video"], "best")
         video_box.pack_start(v_format_label, False, False, 0)
         video_box.pack_start(self.v_format_combo, False, False, 0)
         
-        self.video_frame.add(video_box)
         main_box.pack_start(self.video_frame, False, False, 0)
         
-        # Audio Quality & Format Section
-        audio_frame = Gtk.Frame(label="Audio Settings")
-        audio_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
-        audio_box.set_margin_top(10)
-        audio_box.set_margin_bottom(10)
-        audio_box.set_margin_start(10)
-        audio_box.set_margin_end(10)
+        # Audio Settings Frame
+        audio_frame, audio_box = self.create_frame("Audio Settings")
         
-        a_quality_label = Gtk.Label(label="Audio Quality:", xalign=0)
-        self.a_quality_combo = Gtk.ComboBoxText()
-        self.a_quality_combo.append("best", "Best Available")
-        self.a_quality_combo.append("192", "192 kbps")
-        self.a_quality_combo.append("128", "128 kbps")
-        self.a_quality_combo.append("64", "64 kbps")
-        self.a_quality_combo.set_active_id("best")
+        a_quality_label, self.a_quality_combo = self.create_labeled_combo(
+            "Audio Quality:", self.QUALITY_OPTIONS["audio"], "best")
         audio_box.pack_start(a_quality_label, False, False, 0)
         audio_box.pack_start(self.a_quality_combo, False, False, 0)
         
-        a_format_label = Gtk.Label(label="Audio Format:", xalign=0)
-        self.a_format_combo = Gtk.ComboBoxText()
-        self.a_format_combo.append("best", "Best (auto)")
-        self.a_format_combo.append("mp3", "MP3")
-        self.a_format_combo.append("m4a", "M4A")
-        self.a_format_combo.append("wav", "WAV")
-        self.a_format_combo.append("opus", "Opus")
-        self.a_format_combo.set_active_id("best")
+        a_format_label, self.a_format_combo = self.create_labeled_combo(
+            "Audio Format:", self.FORMAT_OPTIONS["audio"], "best")
         audio_box.pack_start(a_format_label, False, False, 0)
         audio_box.pack_start(self.a_format_combo, False, False, 0)
         
         self.thumbnail_check = Gtk.CheckButton(label="Embed Thumbnail in Audio")
-        self.thumbnail_check.set_active(False)
         audio_box.pack_start(self.thumbnail_check, False, False, 0)
         
-        audio_frame.add(audio_box)
         main_box.pack_start(audio_frame, False, False, 0)
         
-        # Subtitle Settings
-        sub_frame = Gtk.Frame(label="Subtitle Settings")
-        sub_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
-        sub_box.set_margin_top(10)
-        sub_box.set_margin_bottom(10)
-        sub_box.set_margin_start(10)
-        sub_box.set_margin_end(10)
+        # Subtitle Settings Frame
+        sub_frame, sub_box = self.create_frame("Subtitle Settings")
         
         self.subtitle_check = Gtk.CheckButton(label="Download Subtitles")
-        self.subtitle_check.set_active(False)
         self.subtitle_check.connect("toggled", self.on_subtitle_toggled)
         sub_box.pack_start(self.subtitle_check, False, False, 0)
         
         self.embed_sub_check = Gtk.CheckButton(label="Embed Subtitles in Video")
-        self.embed_sub_check.set_active(False)
         self.embed_sub_check.set_sensitive(False)
         sub_box.pack_start(self.embed_sub_check, False, False, 0)
         
-        sub_lang_label = Gtk.Label(label="Subtitle Language:", xalign=0)
-        self.sub_lang_combo = Gtk.ComboBoxText()
-        self.sub_lang_combo.append("en", "English")
-        self.sub_lang_combo.append("all", "All Available")
-        self.sub_lang_combo.append("es", "Spanish")
-        self.sub_lang_combo.append("fr", "French")
-        self.sub_lang_combo.append("de", "German")
-        self.sub_lang_combo.append("hi", "Hindi")
-        self.sub_lang_combo.set_active_id("en")
+        sub_lang_label, self.sub_lang_combo = self.create_labeled_combo(
+            "Subtitle Language:", self.SUB_LANGS, "en")
         self.sub_lang_combo.set_sensitive(False)
         sub_box.pack_start(sub_lang_label, False, False, 0)
         sub_box.pack_start(self.sub_lang_combo, False, False, 0)
         
-        sub_frame.add(sub_box)
         main_box.pack_start(sub_frame, False, False, 0)
         
         # Download Location
@@ -194,19 +174,14 @@ class YtDlpGUI(Gtk.ApplicationWindow):
         self.video_frame.hide()
         
     def on_type_changed(self, combo):
-        download_type = combo.get_active_id()
-        if download_type == "video":
-            self.video_frame.show_all()
-            self.embed_sub_check.set_sensitive(True)
-        else:
-            self.video_frame.hide()
-            self.embed_sub_check.set_sensitive(False)
+        is_video = combo.get_active_id() == "video"
+        self.video_frame.set_visible(is_video)
+        self.embed_sub_check.set_sensitive(is_video and self.subtitle_check.get_active())
     
     def on_subtitle_toggled(self, check):
         is_active = check.get_active()
         self.sub_lang_combo.set_sensitive(is_active)
-        if self.type_combo.get_active_id() == "video":
-            self.embed_sub_check.set_sensitive(is_active)
+        self.embed_sub_check.set_sensitive(is_active and self.type_combo.get_active_id() == "video")
     
     def on_browse_clicked(self, button):
         dialog = Gtk.FileChooserDialog(
@@ -214,13 +189,10 @@ class YtDlpGUI(Gtk.ApplicationWindow):
             parent=self,
             action=Gtk.FileChooserAction.SELECT_FOLDER
         )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OK, Gtk.ResponseType.OK
-        )
+        dialog.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                          Gtk.STOCK_OK, Gtk.ResponseType.OK)
         
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
+        if dialog.run() == Gtk.ResponseType.OK:
             self.location_entry.set_text(dialog.get_filename())
         dialog.destroy()
     
@@ -235,8 +207,7 @@ class YtDlpGUI(Gtk.ApplicationWindow):
             self.show_error("Please enter a URL")
             return
         
-        self.download_thread = threading.Thread(target=self.start_download, args=(url,))
-        self.download_thread.daemon = True
+        self.download_thread = threading.Thread(target=self.start_download, args=(url,), daemon=True)
         self.download_thread.start()
     
     def on_cancel_clicked(self, button):
@@ -244,60 +215,56 @@ class YtDlpGUI(Gtk.ApplicationWindow):
             self.process.terminate()
             GLib.idle_add(self.log_status, "[CANCELLED] Download stopped by user")
     
-    def start_download(self, url):
-        GLib.idle_add(self.log_status, f"Starting download from: {url}")
-        GLib.idle_add(self.progress_bar.set_fraction, 0)
-        
-        download_type = self.type_combo.get_active_id()
-        is_playlist = self.playlist_check.get_active()
-        location = self.location_entry.get_text()
-        
+    def build_cmd(self, url):
+        """Build yt-dlp command based on settings"""
         cmd = ["yt-dlp"]
         
-        if is_playlist:
+        if self.playlist_check.get_active():
             cmd.append("-i")
         
+        download_type = self.type_combo.get_active_id()
+        location = self.location_entry.get_text()
+        
         if download_type == "audio":
-            cmd.extend(["-x", "--audio-format", self.a_format_combo.get_active_id()])
-            quality = self.a_quality_combo.get_active_id()
-            if quality != "best":
-                cmd.extend(["-S", f"aext:m4a/mp3/best"])
-            else:
-                cmd.append("-S")
-                cmd.append("aext:m4a/mp3/best")
-            
+            cmd.append("-x")
+            audio_format = self.a_format_combo.get_active_id()
+            if audio_format != "best":
+                cmd.extend(["--audio-format", audio_format])
+            audio_quality = self.a_quality_combo.get_active_id()
+            if audio_quality != "best":
+                cmd.extend(["--audio-quality", audio_quality])
             if self.thumbnail_check.get_active():
                 cmd.append("--embed-thumbnail")
         else:
             quality = self.quality_combo.get_active_id()
-            v_format = self.v_format_combo.get_active_id()
-            
             if quality == "best":
-                cmd.append("-f")
-                cmd.append("bestvideo+bestaudio/best")
+                cmd.extend(["-f", "bestvideo*+bestaudio/best"])
             else:
-                cmd.append("-f")
-                cmd.append(f"best[height<={quality}]+bestaudio/best")
+                cmd.extend(["-f", f"bestvideo[height<={quality}]+bestaudio/best"])
             
+            v_format = self.v_format_combo.get_active_id()
             if v_format != "best":
-                cmd.extend(["--merge-output-format", v_format])
+                cmd.extend(["--remux-video", v_format])
         
         if self.subtitle_check.get_active():
+            cmd.append("--write-subs")
             sub_lang = self.sub_lang_combo.get_active_id()
-            cmd.extend(["--write-subs", "--sub-langs", sub_lang])
-            
+            cmd.extend(["--sub-langs", sub_lang])
             if self.embed_sub_check.get_active() and download_type == "video":
                 cmd.append("--embed-subs")
         
         cmd.extend(["-o", f"{location}/%(title)s.%(ext)s", url])
+        return cmd
+    
+    def start_download(self, url):
+        GLib.idle_add(self.log_status, f"Starting download from: {url}")
+        GLib.idle_add(self.progress_bar.set_fraction, 0)
         
         try:
+            cmd = self.build_cmd(url)
             self.process = subprocess.Popen(
-                cmd,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                universal_newlines=True,
-                bufsize=1
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                universal_newlines=True, bufsize=1
             )
             
             for line in self.process.stdout:
@@ -307,8 +274,8 @@ class YtDlpGUI(Gtk.ApplicationWindow):
                 if "%" in line and "ETA" in line:
                     try:
                         percent = float(line.split("%")[0].split()[-1])
-                        GLib.idle_add(self.progress_bar.set_fraction, percent / 100)
-                    except:
+                        GLib.idle_add(self.progress_bar.set_fraction, min(percent / 100, 1.0))
+                    except (ValueError, IndexError):
                         pass
             
             self.process.wait()
@@ -316,7 +283,7 @@ class YtDlpGUI(Gtk.ApplicationWindow):
             GLib.idle_add(self.progress_bar.set_fraction, 1.0)
             
         except FileNotFoundError:
-            GLib.idle_add(self.show_error, "yt-dlp not found. Please install it: pip install yt-dlp")
+            GLib.idle_add(self.show_error, "yt-dlp not found. Install: pip install yt-dlp")
         except Exception as e:
             GLib.idle_add(self.show_error, f"Error: {str(e)}")
         finally:
@@ -324,11 +291,9 @@ class YtDlpGUI(Gtk.ApplicationWindow):
     
     def show_error(self, message):
         dialog = Gtk.MessageDialog(
-            transient_for=self,
-            flags=0,
+            transient_for=self, flags=0,
             message_type=Gtk.MessageType.ERROR,
-            buttons=Gtk.ButtonsType.OK,
-            text=message
+            buttons=Gtk.ButtonsType.OK, text=message
         )
         dialog.run()
         dialog.destroy()
